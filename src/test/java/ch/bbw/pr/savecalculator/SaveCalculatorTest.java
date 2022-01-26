859b0aa14165d61e365d07dd02a8ebee9f98ab1c
David <david.kresic@swisscom.com>
2022-01-26 18:36:54 +0100

    Added @Before and the method setUp

--- src/test/java/ch/bbw/pr/savecalculator/SaveCalculatorTest.java
+++ src/test/java/ch/bbw/pr/savecalculator/SaveCalculatorTest.java
@@ -1,15 +1,22 @@
 package ch.bbw.pr.savecalculator;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SaveCalculatorTest {
 
+    private SaveCalculator testee;
+
+    @Before
+    public void setUp() {
+        testee = new SaveCalculator();
+    }
+
     //summe
 
     @Test
     public void testSumZweiPositiveIsOk() {
-        SaveCalculator testee = new SaveCalculator();
         int value1 = 10;
         int value2 = 20;
         assertEquals(30, testee.summe(value1, value2));
@@ -19,7 +26,6 @@ public class SaveCalculatorTest {
 
     @Test
     public void subtraktion() {
-        SaveCalculator testee = new SaveCalculator();
         int value1 = 10;
         int value2 = 20;
         assertEquals(testee.subtraktion(value1, value2), -10);
@@ -27,7 +33,6 @@ public class SaveCalculatorTest {
 
     @Test
     public void testSubZweiNegativeIstOk() {
-        SaveCalculator testee = new SaveCalculator();
         int value1 = 10;
         int value2 = 20;
         assertEquals(-10, testee.subtraktion(value1, value2));
