6f96cadb1cf8f5647a4b03d71ddcdad45d3bb14f
David <david.kresic@swisscom.com>
2022-01-26 18:31:43 +0100

    Added 2 tests for Subtraktion

--- src/test/java/ch/bbw/pr/savecalculator/SaveCalculatorTest.java
+++ src/test/java/ch/bbw/pr/savecalculator/SaveCalculatorTest.java
@@ -5,11 +5,31 @@ import static org.junit.Assert.*;
 
 public class SaveCalculatorTest {
 
+    //summe
+
     @Test
     public void testSumZweiPositiveIsOk() {
         SaveCalculator testee = new SaveCalculator();
         int value1 = 10;
         int value2 = 20;
         assertEquals(30, testee.summe(value1, value2));
+    }
+
+    //subtraktion
+
+    @Test
+    public void subtraktion() {
+        SaveCalculator testee = new SaveCalculator();
+        int value1 = 10;
+        int value2 = 20;
+        assertEquals(testee.subtraktion(value1, value2), -10);
+    }
+
+    @Test
+    public void testSubZweiNegativeIstOk() {
+        SaveCalculator testee = new SaveCalculator();
+        int value1 = 10;
+        int value2 = 20;
+        assertEquals(-10, testee.subtraktion(value1, value2));
     }
 }
